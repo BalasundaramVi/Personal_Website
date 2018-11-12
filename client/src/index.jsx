285efadb9921259bcd5bcda4782be2a336e52521
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-12 14:31:15 -0800

    Add intro cover image

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
+  <h1>Header</h1>,
+  document.getElementById('intro')
+);
+
+ReactDOM.render(
   <h1>Hello World</h1>,
   document.getElementById('root')
 );
